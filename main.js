d9cf1deade8ab48bf499a215833436727efbb86b
skjeeth1 <skjeeth1@gmail.com>
2025-01-11 22:24:38 +0530

    Finished Work. Need to add meaningful UI

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ document.querySelector("form").addEventListener(
             data.get("count_pages"),
             data.get("read_check")
         );
-        
+        e.target.reset();
         console.log(data.get("read_check"));
         dialogEl.close();
     }
@@ -28,25 +28,48 @@ function Book (title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.button = null;
+    this.domEl = null;
 
     this.createDomElement = function () {
         const main = document.createElement("div");
         const title = document.createElement("div");
         const author = document.createElement("div");
         const pages = document.createElement("div");
-        const read = document.createElement("div");
+        const read_card = document.createElement("div");
+        const read = document.createElement("button");
+        const rem = document.createElement("button");
+
+        rem.addEventListener('click', (e) => {
+            removeFromLibrary(this.title);
+        });
+
+        this.button = rem;
+        this.domEl = main;
 
         title.innerText = this.title;
         author.innerText = this.author;
-        pages.innerText = this.pages;
-        read.innerText = this.read ? "Read" : "Not read";
+        pages.innerText = this.pages + "pages";
+        read_card.innerText = "Status: " + (this.read ? "Read" : "Not read");
+        read.innerText = !this.read ? "Read?" : "Not read?";
+        rem.innerText = "Remove";
 
         main.appendChild(title);
         main.appendChild(author);
         main.appendChild(pages);
+        main.appendChild(read_card);
         main.appendChild(read);
-
+        main.appendChild(rem);
+        
+        main.classList.add("card");
+        read.classList.add("define")
         cont.appendChild(main);
+
+        read.addEventListener('click', (e) => {
+            this.read = !this.read;
+            e.target.innerText = !this.read ? "Read?" : "Not read?";
+        })
+
     }
 }
 
@@ -54,4 +77,13 @@ function addToBookLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     book.createDomElement();
     library.push(book);
+}
+
+function removeFromLibrary(title) {
+    for (let i = library.length - 1; i >= 0; i--) {
+        if (library[i].title == title) {
+            cont.removeChild(library[i].domEl);
+            library.splice(i);
+        }
+    }
 }
